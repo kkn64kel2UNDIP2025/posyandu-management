de9508abdcda0c79fad2bd7ad4aaa2b1eb30bc0c
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2025-07-17 13:55:16 +0700

    Clear header and add button for xlsx download

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -33,9 +33,9 @@
 </div>
 
 <footer>
-    <p class="text-base text-gray-400 font-normal p-3 text-center">
-        Design and Developed by <a href="https://www.wrappixel.com/"
-            class="text-blue-600 underline hover:text-blue-700">wrappixel.com</a>
+    <p class="text-gray-400 font-normal my-3 text-center text-sm">
+        Dibuat dan dikembangkan oleh <a href="https://www.instagram.com/storyofkaligawe_/" target="_blank"
+            class="block text-blue-600 underline hover:text-blue-700">KKN Tim 64 Kelompok 2 UNDIP 2025</a>
     </p>
 </footer>
 <!-- Main Content End -->
